fd7956b9e68f8442aa7dcb37002a42302511ea4d
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-04-24 10:20:05 -0400

    Update gayr8.js

--- DiscordBot/Fun/gayr8.js
+++ DiscordBot/Fun/gayr8.js
@@ -10,8 +10,16 @@ min = Math.ceil(1);
 max = Math.floor(100);
 const gayrate = Math.floor(Math.random() * (max - min + 1)) + min;
 
- message.channel.send(`You are ${gayrate}% gay`);
- if(gayrate < 50) then(message.channel.send('nigga ur gay'));
+if(gayrate < 99) return (message.channel.send(
+    'nigga ur 100% gay'
+));
+ 
+if(gayrate = 69) return (message.channel.send(
+    'bruh ur 69% gay'
+));
+
+message.channel.send(`You are ${gayrate}% gay`);
+
     
 
     
