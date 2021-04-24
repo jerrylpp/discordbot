36946463bea7d447994dc7f68052fd227f937bc6
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-04-24 10:12:30 -0400

    dafg

--- DiscordBot/Fun/gayr8.js
+++ DiscordBot/Fun/gayr8.js
@@ -11,8 +11,9 @@ max = Math.floor(100);
 const gayrate = Math.floor(Math.random() * (max - min + 1)) + min;
 
  message.channel.send(`You are ${gayrate}% gay`);
+ if('${gayrate}' < 50)message.channel.send('nigga ur gay');
     
 
-
+    
 }
 }
