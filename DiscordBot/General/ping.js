5fa98ba4ac4fba5474da11241b546fcffbaf4ad7
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-04-30 21:00:50 -0400

    temp remove ping.js

--- DiscordBot/General/ping.js
+++ DiscordBot/General/ping.js
@@ -1,8 +1,8 @@
-module.exports = {
-    name: 'ping',
-    description: 'nah',
-    execute(message, args){
-        message.channel.send(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    }
+// module.exports = {
+//     name: 'ping',
+//     description: 'nah',
+//     execute(message, args){
+//         message.channel.send(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+//     }
         
-}
+// }
