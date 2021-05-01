13be73dd6753101db5038d1e40a4be4721a95a06
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-04-30 20:30:15 -0400

    fixed some typos and shit

--- DiscordBot/Fun/penis.js
+++ DiscordBot/Fun/penis.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'penis',
     description: 'description',
-    execute(messaage, args){
+    execute(message, args){
         message.channel.send('penis')
     }
 }
