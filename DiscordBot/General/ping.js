180551f842cb5a6cfa366906a7c15f95781f76ea
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-04-30 20:51:53 -0400

    ping.js 2.0

--- DiscordBot/General/ping.js
+++ DiscordBot/General/ping.js
@@ -2,6 +2,10 @@ module.exports = {
     name: 'ping',
     description: 'nah',
     execute(message, args){
-        message.channel.send(`Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        message.channel.send('Loading data').then (async (msg) =>{
+        msg.delete()
+        message.channel.send(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
+        )
+}
 }
