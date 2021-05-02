d2748da353872769abe51b56fcf357ab1d446087
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-05-02 13:19:01 -0400

    ping.js

--- DiscordBot/General/ping.js
+++ DiscordBot/General/ping.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'nah',
     execute(message, args){
         message.reply('Calculating ping...').then((resultMessage) => {
+            const client = new Discord.Client();
             const ping = resultMessage.createdTimestamp - message.createdTimestamp
 
             message.reply(`Bot latency: ${ping}, API latency: ${client.ws.ping}`)
