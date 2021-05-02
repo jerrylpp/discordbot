b35278948ab2a12bb06591fd97958e8e6f2e5d3b
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-05-02 11:50:41 -0400

    penis.js

--- DiscordBot/Fun/penis.js
+++ DiscordBot/Fun/penis.js
@@ -4,6 +4,12 @@ module.exports = {
     name: 'penis',
     description: 'description',
     execute(message, args){
-        message.channel.send('penis')
+        min = Math.ceil(2);
+        max = Math.floor(10);
+        const length = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+        message.channel.send(`your penis is ${length} inches long`)
+        
+    
     }
 }
