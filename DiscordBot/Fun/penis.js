3b4ae300690fd758ad8de1eae5c870b807eccaf8
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-05-06 10:55:44 -0400

    penis.js

--- DiscordBot/Fun/penis.js
+++ DiscordBot/Fun/penis.js
@@ -8,7 +8,7 @@ module.exports = {
         max = Math.floor(12);
         const length = Math.floor(Math.random() * (max - min + 1)) + min;
     if(length == 2)message.channel.send(`${message.author.username}'s penis: 8==D`);
-    if(length == 3)message.channel.send('ur penis: 8===D');
+    if(length == 3)message.channel.send(`${message.author.username}'s penis: 8===D`);
     if(length == 4)message.channel.send('ur penis: 8====D');
     if(length == 5)message.channel.send('ur penis: 8=====D');
     if(length == 6)message.channel.send('ur penis: 8======D');
@@ -16,7 +16,7 @@ module.exports = {
     if(length == 8)message.channel.send('ur penis: 8========D');
     if(length == 9)message.channel.send('ur penis: 8=========D');
     if(length == 10)message.channel.send('ur penis: 8==========D');
-    if(length == 11)message.channel.send('ur penis: 8===========D');
+    if(length == 11)message.channel.send(`${message.author.username}'s penis: 8===========D`);
     if(length == 12)message.channel.send('ur penis: 8============D');
 
 
