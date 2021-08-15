66a7ec4e939727f5ffee8c8c4b815772d919d7cd
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-08-15 14:17:54 -0400

    updated liam.js

--- DiscordBot/Fun/liam.js
+++ DiscordBot/Fun/liam.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "liam",
     descritption: "null",
     execute(message, args){
-        message.channel.send('hes crip killa unpog')
+        message.channel.send('liam lieks men')
     }
 }
