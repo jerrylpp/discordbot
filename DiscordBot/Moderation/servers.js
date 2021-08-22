61aa473c55af33505e558975dac3a05814d4f57a
Penguin6969 <72304684+Penguin6969@users.noreply.github.com>
2021-08-22 14:52:19 -0400

    Update servers.js

--- DiscordBot/Moderation/servers.js
+++ DiscordBot/Moderation/servers.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 
 module.exports = {
-    name: 'server',
+    name: 'servers',
     description: "lists servers the bot is in through console.",
     execute(message, args){
 
